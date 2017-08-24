e02a41f054bff0c0be1a4dd93f3fcced0a38323f
hklliang <304978040@qq.com>
2017-08-24 08:28:01 +0800

    24

--- rabbitMQ/receive.py
+++ rabbitMQ/receive.py
@@ -1,7 +1,7 @@
 # _*_coding:utf-8_*_
 __author__ = 'Alex Li'
 import pika
-
+import time
 connection = pika.BlockingConnection(
     pika.ConnectionParameters('localhost'))
 channel = connection.channel()
@@ -11,15 +11,19 @@ channel = connection.channel()
 # was run before. But we're not yet sure which program to run first. In such cases it's a good
 # practice to repeat declaring the queue in both programs.
 channel.queue_declare(queue='hello')
-
+#队列，如果生产者没有队列，消费者可以生成队列
+# channel.queue_declare(queue='hello',durable=True)#队列持久化，durable=True需要在生产者和消费者都加上
 
 def callback(ch, method, properties, body):
+    print("--->",ch,method,properties)
+    time.sleep(10)
     print(" [x] Received %r" % body)
+    ch.basic_ack(delivery_tag=method.delivery_tag)#需要确认消息已经收到，否则一起动又会自动收消息
 
-
+channel.basic_qos(prefetch_count=1)#只能处理一条消息，没有处理完，不会给他发消息
 channel.basic_consume(callback,
-                      queue='hello',
-                      no_ack=True)
+                      queue='hello',)
+                      # no_ack=True)#确认消息，一般为False，需要确认消息
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
--- rabbitMQ/send.py
+++ rabbitMQ/send.py
@@ -11,7 +11,11 @@ channel.queue_declare(queue='hello')
 
 # n RabbitMQ a message can never be sent directly to the queue, it always needs to go through an exchange.
 channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body='Hello World!')
+                      routing_key='hello',#queue名字
+                      body='Hello World!',
+                      properties=pika.BasicProperties(delivery_mode=2)#make message persistent
+                      )
+
+#如果只有消息持续化，队列没有持续化，也是不会持续化
 print(" [x] Sent 'Hello World!'")
 connection.close()
